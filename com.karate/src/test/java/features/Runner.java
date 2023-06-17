368aadd9a17feaec746660952df836da18c04bf6
Kaizar Akib Ayon <kizar.cse@gmail.com>
2023-06-17 13:23:48 +0600

    runner class updated

--- com.karate/src/test/java/features/Runner.java
+++ com.karate/src/test/java/features/Runner.java
@@ -8,4 +8,18 @@ public class Runner {
     Karate apiGETresponseTest(){
         return Karate.run("api").relativeTo(getClass());
     }
+
+    @Karate.Test
+    Karate apiPOSTresponseTest(){
+        return Karate.run("postApi").relativeTo(getClass());
+    }
+    @Karate.Test
+    Karate apiPUTresponseTest(){
+        return Karate.run("put_Api").relativeTo(getClass());
+    }
+    @Karate.Test
+    Karate apiE2ETestingresponseTest(){
+        return Karate.run("e2e_apiTesting").relativeTo(getClass());
+    }
 }
+
